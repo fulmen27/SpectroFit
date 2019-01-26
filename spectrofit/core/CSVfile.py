ea4db5dbbed7211f21e43f1f3c695a199b2d7add
Unknown <fulminata27@gmail.com>
2019-01-26 15:39:33 +0100

    open files
    - open csv
    - open json

--- spectrofit/core/CSVfile.py
+++ spectrofit/core/CSVfile.py
@@ -1,5 +1,7 @@
 import csv
+import json
 import os
+from pprint import pprint
 
 from tkinter.filedialog import askopenfilename
 
@@ -7,11 +9,12 @@ from tkinter.filedialog import askopenfilename
 class ImportCSV:
     def __init__(self):
         print("import_csv")
-        self.my_csv = {}
-        self.delim = {}
+        self.my_csv = dict()
+        self.delim = dict()
+        self.data = {"lambda": [], "yspectre": [], "3rd_col": []}
         self.delim["filename"] = "delim_ordre_TBL.json"
         self._open_csv()
-        self._open_deim()
+        self._open_delim()
 
     def __str__(self):
         print(self.my_csv)
@@ -22,10 +25,17 @@ class ImportCSV:
 
         if filename != "" and filename is not None and os.path.exists(filename):
             self.my_csv["filename"] = filename
-            with open(self.my_csv["filename"], 'rb') as self.my_csv["filename"]:
-                self.my_csv["csv_file"] = csv.reader(self.my_csv["filename"], delimiter=' ', quotechar='|')
+            with open(self.my_csv["filename"], 'r') as self.my_csv["filename"]:
+                self.my_csv["csv_file"] = csv.reader(self.my_csv["filename"], delimiter=";")
+                for row in self.my_csv["csv_file"]:
+                    self.data["lambda"].append(row[0])
+                    self.data["yspectre"].append(row[1])
+                    self.data["3rd_col"].append(row[2])
 
     def _open_delim(self):
-        #ouvrir et classer les delimitations des ordres
-        with open(self.delim["filename"], 'rb') as self.delim["filename"]:
-            self.delim["csv_file"] = csv.reader(self.delim["filename"], delimiter=' ', quotechar='|')
+        # ouvrir et classer les delimitations des ordres
+        self.delim["filename"] = "./spectrofit/core/delim_ordre_TBL.json"
+        self.delim["delim_file"] = open(self.delim["filename"])
+        self.delim["delim_data"] = json.load(self.delim["delim_file"])
+        # pprint(self.delim["delim_data"])
+        # print(self.delim["delim_data"]['ordre_32']['lim_basse'])
